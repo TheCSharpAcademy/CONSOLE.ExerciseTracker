45cc3b41ddf0c8995b81eb684739e6e9889bd6a1
Pablo Souza <cappuccinocodes@gmail.com>
2021-12-18 09:11:34 +1000

    started repository

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using ExerciseTracker.Api;
+using ExerciseTracker.Api.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IExerciseRepository, ExerciseRepository>();
+
 
 builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Initial Catalog=Exercises; Integrated Security=true;"));
---
+++ Repositories/ExerciseRepository.cs
@@ -0,0 +1,60 @@
+﻿using ExerciseTracker.Api.Models;
+
+namespace ExerciseTracker.Api.Repositories
+{
+    public interface IExerciseRepository
+    {
+        IEnumerable<Exercise> GetAll(int id);
+        Exercise GetById(int id);
+        void Add(Exercise exercise);
+        void Delete(int id);
+        void Update(Exercise exercise);
+        bool Save();
+    }
+    public class ExerciseRepository: IExerciseRepository
+    {
+        private readonly DataContext _context;
+
+        public ExerciseRepository(DataContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public void Add(Exercise exercise)
+        {
+            if (exercise == null)
+            {
+                throw new ArgumentNullException(nameof(Exercise));
+            }
+
+            _context.Exercises.Add(exercise);
+        }
+
+        public void Delete(Exercise exercise)
+        {
+            _context.Exercises.Remove(exercise);
+        }
+
+        public IEnumerable<Exercise> GetAll(int id)
+        {
+            return _context.Exercises
+            .OrderBy(s => s.Id)
+            .ToList();
+        }
+
+        public Exercise GetById(int id)
+        {
+            return _context.Exercises.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Exercise exercise)
+        {
+            _context.Exercises.Update(exercise);
+        }
+
+        public bool Save()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}
